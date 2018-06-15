1d6f70f785e3363939ae5f8d115c540df0682f9e
ayako.mitsui <ayako.b.mitsui@rakuten.com>
2018-06-15 03:57:40 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,15 +6,26 @@
 
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-md-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステイタス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
 
-        {!! Form::label('status', 'ステイタス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
 
 @endsection
